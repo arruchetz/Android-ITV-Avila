be49b344ba929e8ab45267734476bc6ab38380e5
arruchetz <dgarruche@gmail.com>
2023-01-16 18:59:58 +0100

    ITV Avila

---
+++ app/src/main/java/com/danielgararr/itv_avila/MainActivity.java
@@ -0,0 +1,14 @@
+package com.danielgararr.itv_avila;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
